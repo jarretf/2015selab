2c6fed35d80774331ddd446182f3332187c16952
jarretf <jarret.java@gmail.com>
2015-06-23 15:38:31 +0530

    3) added 2nd line of code

--- HelloJarret/src/testPackage/MainPrg.java
+++ HelloJarret/src/testPackage/MainPrg.java
@@ -5,6 +5,7 @@ public class MainPrg {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello jarret");
+		System.out.println("thank you jarret");
 	}
 
 }
